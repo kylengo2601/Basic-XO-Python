e8f85867c23dcb3d54709fe1ea474b7625b83add
kylengo2601 <55031830+kylengo2601@users.noreply.github.com>
2023-06-03 00:55:53 +0700

    Finish code for Lesson 1

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -20,6 +20,8 @@
     Trong trường hợp cả 2 không thể thắng vì đã hết nước đi, hai người chơi sẽ hòa.
 """
 
+# As a beginning state, write a function to return a 3x3 matrix with all space - " " 
+# characters.
 def create_board():
     return [
     [" ", " ", " "],
@@ -27,7 +29,10 @@ def create_board():
     [" ", " ", " "]
     ]
 
-def print_board(board):
+board = create_board()
+
+# Given
+def print_board():
     cols = " " * 3
     for x in range(len(board[0])):
         cols += str(x) + " "
@@ -40,5 +45,18 @@ def print_board(board):
         print(board_line)
 
 
-board = create_board()
-print_board(board)
+# Write a function to either place an X or O to the specified position on the board.
+def make_play(player, y_coor, x_coor):
+    board[y_coor][x_coor] = player
+    print_board()
+
+# Sample manual play
+# make_play("O", 1, 1)
+# make_play("X", 1, 2)
+# make_play("O", 0, 0)
+# make_play("X", 2, 2)
+# make_play("O", 0, 2)
+# make_play("X", 2, 0)
+# make_play("O", 2, 1)
+# make_play("X", 0, 1)
+# make_play("O", 1, 0)
